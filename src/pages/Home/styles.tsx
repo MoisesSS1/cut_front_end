4300a228f309e7820b47ebee30d05c84349a7687
MoisesSS1 <investmentmoises@gmail.com>
2023-10-31 01:57:49 -0300

    corrigindo home

--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -1,15 +1,12 @@
 import styled from "styled-components/native";
-<<<<<<< HEAD
+
 import global from "../../global/global";
-=======
->>>>>>> 8369642cdb5d159d5165cfb62f14193459297996
 
 export const Container = styled.View`
   flex: 1;
 `;
 
 export const BoxAdress = styled.View`
-<<<<<<< HEAD
   margin: 15px 20px;
   padding: 0px 20px;
   border-radius: 5px;
@@ -25,18 +22,10 @@ export const TextAdress = styled.TextInput`
   margin-left: 10px;
   font-size: 15px;
   font-weight: ${global.theme.font.bold};
-=======
-  height: 13%;
-  background-color: #dfb278;
->>>>>>> 8369642cdb5d159d5165cfb62f14193459297996
 `;
 
 export const Content = styled.View`
   margin: 20px 10px;
   height: 72%;
-<<<<<<< HEAD
   background-color: ${global.theme.colors.white};
-=======
-  background-color: #48a7a7;
->>>>>>> 8369642cdb5d159d5165cfb62f14193459297996
 `;
